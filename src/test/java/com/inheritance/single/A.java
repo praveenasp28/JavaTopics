be66ad6d64c15f8f7c8f4f14503cb414a5d19218
praveenasp28 <praveenasp28@gmail.com>
2025-12-24 18:55:47 +0530

    created by tester2

--- src/test/java/com/inheritance/single/A.java
+++ src/test/java/com/inheritance/single/A.java
@@ -5,6 +5,10 @@ public class A {
 	public void courseName() {
 
 		System.out.println("Java");
+		System.out.println("Selenium");
+		System.out.println("API");
+		System.out.println("MobileTesting");
+		System.out.println("ManualTesting");
 	}
 
 }
